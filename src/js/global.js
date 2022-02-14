4dd5c555352ca518ad48c3889219ee9ece9c1875
gtasa3box <gtasa3box@gmail.com>
2022-02-14 16:37:18 +0300

    com

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,16 @@
-console.log('global');
+
+$('.btn_catalog').click(function(){
+    $('.btn_catalog').toggleClass("active");
+    $('.catalog-popup').toggleClass("active");
+    $('.shadow-block').toggleClass("active");
+});
+
+
+// catalog-drop-elems
+$(".catalog-popup__nav-elem").hover(function(){
+    $('.catalog-popup__nav-elem').removeClass('active');
+    $(this).addClass("active");
+    $('.catalog-popup__nav-child-block').removeClass('active');
+    $($(this).data("target")).addClass("active");
+});
+
